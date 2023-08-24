bc958cdec83c11e63e3297ad94f86314f7e4adf6
tyoanantyo <build.anantyo@gmail.com>
2023-08-25 05:25:26 +0700

    tailwindcss project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.{html,js}'],
+  content: ['./dist/**/*.{html,js}', 'index.html'],
   theme: {
     container: {
       center: true,

b10505365e0ee88a9f5e0bccc53610091e360811
tyoanantyo <build.anantyo@gmail.com>
2023-08-25 04:08:28 +0700

    Modified tailwindcss project top icon

--- public/js/script.js
+++ public/js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function(){
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
     if(window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
 }
 
@@ -17,4 +22,13 @@ const navMenu = document.querySelector('#nav-menu');
 hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
+});
+
+// Click Outside
+window.addEventListener('click', function(e) {
+    if( e.target !=hamburger &&  e.target != navMenu ) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+
+    }
 });
